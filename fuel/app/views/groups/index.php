9cb19c56d98a69ad83e438553445969678e5b403
Alessio Lunghi <alessio@gamaroff.co.uk>
2013-05-15 20:51:27 +0100

    circles on feed page

--- fuel/app/views/groups/index.php
+++
@@ -1,7 +0,0 @@
-<ul class="nav nav-pills">
-	<li class='<?php echo Arr::get($groupnav, "list" ); ?>'><?php echo Html::anchor('/groups/list','<i class="icon-th-list"></i>');?></li>
-	<li class='<?php echo Arr::get($groupnav, "create" ); ?>'><?php echo Html::anchor('/groups/create','<i class="icon-plus-sign"></i>');?></li>
-	<li class='<?php echo Arr::get($groupnav, "invite" ); ?>'><?php echo Html::anchor('/groups/invite','<i class="icon-flag"></i>');?></li>
-</ul>
-
-<p>Index</p>
